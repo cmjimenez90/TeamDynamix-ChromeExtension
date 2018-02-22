21fb2901b6455822205f594becbcd60dd960e755
Carlos Jimenez <c.m.jimenez90@gmail.com>
2018-02-21 19:14:26 -0500

    add alarm reactivation on load configuration based on persistence.
    fix value bug for value returning as NaN.

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function alarmModification(value){
     console.log("clearing alarm");
     chrome.alarms.clear(alarmName);
     console.log("alarm cleared");
-    if(value !== 0){
+    if(value){
         if(value !== -1){
             console.log("creating alarm");
             chrome.alarms.create(alarmName,{periodInMinutes : value});
@@ -14,8 +14,7 @@ function alarmModification(value){
             console.log("creating test alarm");
             chrome.alarms.create(alarmName,{periodInMinutes : .5});
             console.log("alarm test created");
-        }
-        
+        }   
     }
     
 };
@@ -23,12 +22,22 @@ function alarmModification(value){
 function loadConfiguration(selector){
     chrome.storage.sync.get('selected',function(selected){
         if(selected.selected){
+            console.log("persistence found");
             for(var option, current = 0; option = selector.options[current]; current++) {
                 if(option.value == selected.selected) {
                     selector.selectedIndex = current;
+                    console.log("setting choice to value: "+ selector.value);
                     break;
                 }
             }
+            console.log("looking for active alarm");
+            chrome.alarms.get('tdRefreshAlarm',function (alarm) {
+                if(!alarm){
+                    console.log("no alarm found, creating alarm based on persistence.");
+                    alarmModification(parseInt(selected));
+                    console.log("loadconfiguration complete: alarm created.");
+                }
+            });
         }
     });
 }
