48dd95f6e7beec38344fa66633c6897d7a46a175
Carlos Jimenez <c.m.jimenez90@gmail.com>
2018-02-22 20:29:10 -0500

    restucture refreshtickets action.
    Will  now refresh desktop view.
    Only refresh if ticket tab is selected.

--- tdtickets_cs.js
+++ tdtickets_cs.js
@@ -3,13 +3,37 @@ console.log("tdtickets_cs.js is running");
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
     console.log("message recieved");
     if(request.action == "refreshTickets"){
-        var iframeDoc = document.getElementsByTagName("iframe")[0].contentWindow.document;
-        var rightIframeDoc = iframeDoc.getElementById("RightFrame").contentWindow.document;
-        console.log(iframeDoc);
-        console.log(rightIframeDoc);
-        var refreshButton = rightIframeDoc.getElementById("btnRefresh");
-        console.log(refreshButton);
-        refreshButton.click();
-        console.log("refresh button clicked");
-        } 
+        if(shouldProcessRequest()){
+            console.log("valid tab selected: performing refreshTicketsAction");
+            var iframeDoc = document.getElementsByTagName("iframe")[0].contentWindow.document;
+            var rightIframeDoc = iframeDoc.getElementById("RightFrame").contentWindow.document;
+            console.log(iframeDoc);
+            console.log(rightIframeDoc);
+            if(getSelectedNavItem(iframeDoc) == "divDesktopHdr"){
+                console.log("Desktop view is selected; getting refresh links");
+                var refreshLinks = rightIframeDoc.getElementsByClassName("fa-refresh");
+                console.log(refreshLinks);
+                for (let link of refreshLinks){
+                    console.log("calling onClick on "+link);
+                    link.click();
+                    console.log("success");
+               }
+            }
+            else{
+                var refreshButton = rightIframeDoc.getElementById("btnRefresh");
+                console.log(refreshButton);
+                refreshButton.click();
+                console.log("refresh button clicked");
+            }  
+        }      
+    } 
 });
+
+function shouldProcessRequest(){
+    return (document.getElementsByClassName("ui-state-active")[0].getAttribute("tabindex") == "0") ? true : false;
+}
+
+function getSelectedNavItem(iframe){
+    return iframe.getElementsByClassName("selected")[0].getAttribute("id");
+}
+
