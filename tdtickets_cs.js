8895d9ebabd81aee3f12461f10120cfe24702e84
Jimenez <cjimene1@rcgc.edu>
2018-02-23 11:03:52 -0500

    Add comments and Adjust whitespace throught out extension.

--- tdtickets_cs.js
+++ tdtickets_cs.js
@@ -1,5 +1,11 @@
 console.log("tdtickets_cs.js is running");
-//waits for refreshTickets message, if received processes refresh.
+/* ******************************************************************************************************************
+*Waits for refreshTickets message, if received processes refresh under the following critia.
+*Detects the selected tab on the main document page. Looks for Ticket tab, if not selected action ends.
+*If ticket tab is selected, refreshes tickets based on view. Refresh logic is different if the Desktop view is active.
+*WARNING: Potentially very volatile. 
+*Since it relies on the current design of the TeamDynamix webpage, updates applied by the TeamDynamix team can potentially break this plug in.
+********************************************************************************************************************/
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
     console.log("message recieved");
     if(request.action == "refreshTickets"){
@@ -9,6 +15,7 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
             var rightIframeDoc = iframeDoc.getElementById("RightFrame").contentWindow.document;
             console.log(iframeDoc);
             console.log(rightIframeDoc);
+            //Process desktop view logic
             if(getSelectedNavItem(iframeDoc) == "divDesktopHdr"){
                 console.log("Desktop view is selected; getting refresh links");
                 var refreshLinks = rightIframeDoc.getElementsByClassName("fa-refresh");
@@ -19,6 +26,7 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
                     console.log("success");
                }
             }
+            //Process any other view
             else{
                 var refreshButton = rightIframeDoc.getElementById("btnRefresh");
                 console.log(refreshButton);
